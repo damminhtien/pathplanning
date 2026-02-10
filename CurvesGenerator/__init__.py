c879ab8d4e8c6d977556a57b60ea93ed355fda38
damminhtien <damminhtienchl@gmail.com>
2026-02-11 05:22:12 +0700

    refactor: merge legacy packages into pathplanning

--- CurvesGenerator/__init__.py
+++ CurvesGenerator/__init__.py
@@ -1,1 +1,7 @@
-"""Curve generation utilities."""
+"""Compatibility shim; code now lives in pathplanning.CurvesGenerator."""
+
+from importlib import import_module
+import sys
+
+_impl = import_module("pathplanning.CurvesGenerator")
+sys.modules[__name__] = _impl
